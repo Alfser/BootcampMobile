6db2026600eff61b627e52a6dc0e14ef64f7c16c
alfser <j.janilson12@gmail.com>
2021-06-25 14:31:09 -0300

    creating class exemple

---
+++ POOKotlin/Pessoa.kt
@@ -0,0 +1,11 @@
+
+class Pessoa{
+    var nome: String = "Janilson"
+    var cpf:String = "333.556.896.532-43"
+}
+
+fun main(){
+    val pessoa = Pessoa()
+    println(pessoa.nome)
+    println(pessoa.cpf)
+}
