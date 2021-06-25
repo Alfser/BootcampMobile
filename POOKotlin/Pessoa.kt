bbfb55861d22355d87dbfeb4b67765185695c50d
alfser <j.janilson12@gmail.com>
2021-06-25 14:36:41 -0300

    adding inner classes

--- POOKotlin/Pessoa.kt
+++ POOKotlin/Pessoa.kt
@@ -2,10 +2,17 @@
 class Pessoa{
     var nome: String = "Janilson"
     var cpf:String = "333.556.896.532-43"
+
+    inner class Endereco{
+        var rua:String = "Dois de Dezembro"
+        var cep:String = "66816-030"
+        var numbero = "7"
+    }
 }
 
 fun main(){
     val pessoa = Pessoa()
     println(pessoa.nome)
     println(pessoa.cpf)
+    println(pessoa.Endereco().rua)
 }
