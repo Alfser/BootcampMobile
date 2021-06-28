df021b77eca216fdd07b5b2a78cec0a39260742c
alfser <j.janilson12@gmail.com>
2021-06-28 10:32:03 -0300

    Arrays operations

--- collections/collectionsArrays.kt
+++ collections/collectionsArrays.kt
@@ -46,6 +46,22 @@ fun funDoubleArray(){
     }
 }
 
+fun salarios(){
+    val salarios = doubleArrayOf(1223.4, 4335.7, 5412.9, 2312.89, 6712.56, 4645.67)
+    
+    println("Menor Salário: ${salarios.minOrNull()}")
+    println("Maior Slário: ${salarios.maxOrNull()}")
+    println("Média Salarial: ${salarios.average()}")
+
+    println("Salários acima de 5 mill: ${salarios.filter{it> 5000.0}}")
+
+    println("Quantidade de salários acima de 5 mill ${salarios.count{it > 5000.0}}")
+
+    println("Salários acima de 5000.0 usando 'find': ${salarios.find{it > 5000.0}}")
+
+    println("Existe salários acima de 5 mill? ${if(salarios.any{it > 5000.0}) "sim" else "não"}")
+}
+
 fun main(){
-    funDoubleArray()
+    salarios()
 }
